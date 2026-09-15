378479a79329525061e61380a8b123b7bcfe9b57
4sigils <xdmakesmebad@gmail.com>
2026-09-14 20:59:10 -0400

    Update strikeanims.lua

--- strikeanims.lua
+++ strikeanims.lua
@@ -12,12 +12,12 @@ return {
     ["138145073296964"] = { pb = true, t = 0.05 }, -- Downslam
     ["117799573414500"] = { pb = true, t = 0.05 }, -- Uptilt
 
-    ["117799573414500"] = { pb = true, t = {0.05, 0.45} }, -- Divergent Fist
+    ["117799573414500"] = { pb = true, t = {0.15, 0.3} }, -- Divergent Fist
     --["130097105243600"] = { pb = true, t = 0.05 }, -- Divergent Fist Air
     --["136537081085390"] = { pb = true, t = 0.05 }, -- Slide Kick
-    ["90746235953469"] = { pb = true, t = 0.05 }, -- Cursed Beatdown
-    ["88425052547737"] = { pb = true, t = 0.05 }, -- Cursed Beatdown Air
-    ["130956157529019"] = { pb = true, t = 0.05 }, -- Manji Kick
+    ["90746235953469"] = { pb = true, t = 0.2 }, -- Cursed Beatdown
+    ["88425052547737"] = { pb = true, t = 0.35 }, -- Cursed Beatdown Air
+    ["130956157529019"] = { pb = true, t = 0.5 }, -- Manji Kick
     
     ----------------------------------------------------------------
     -- Examples
