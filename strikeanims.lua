8df3353c64e1162ca5e6241012ee526b45586401
4sigils <xdmakesmebad@gmail.com>
2026-09-14 21:29:12 -0400

    Update strikeanims.lua

--- strikeanims.lua
+++ strikeanims.lua
@@ -12,7 +12,7 @@ return {
     ["138145073296964"] = { pb = true, t = 0.05 }, -- Downslam
     ["117799573414500"] = { pb = true, t = 0.05 }, -- Uptilt
 
-    ["81458066007277"] = { pb = true, t = {2, 2} }, -- Divergent Fist
+    ["81458066007277"] = { pb = true, t = {.5, .5} }, -- Divergent Fist
 
     --["130097105243600"] = { pb = true, t = 0.05 }, -- Divergent Fist Air
     --["136537081085390"] = { pb = true, t = 0.05 }, -- Slide Kick
