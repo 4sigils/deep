1f799db1a777d507ca83a1dac93ae66f495c9877
4sigils <xdmakesmebad@gmail.com>
2026-09-17 14:27:40 -0400

    Update phoeating.lua

--- phoeating.lua
+++ phoeating.lua
@@ -1,1 +1,10 @@
-
+local ANIMATIONS = {
+    -- // Fighting Styles \\ --
+--  ["13243418555"] = { timing = 0.470, range = 6 },
+    -- Fist
+    ["13243418555"] = { timing = 0.3, range = 6 }, -- M1
+    ["13243419833"] = { timing = 0.3, range = 6 }, -- M2
+    ["13243420683"] = { timing = 0.3, range = 6 }, -- M3
+    ["13243421318"] = { timing = 0.3, range = 6 }, -- M4
+    ["13243421985"] = { timing = 0.3, range = 6 }, -- M5
+}
