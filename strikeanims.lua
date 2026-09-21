7f46faeec152f5ddc4dfe245bbc6388f88325ebd
4sigils <xdmakesmebad@gmail.com>
2026-09-21 09:03:13 -0400

    Update strikeanims.lua

--- strikeanims.lua
+++ strikeanims.lua
@@ -1,12 +1,13 @@
 return {
+    --["rbxassetid://"] = { name = "M1",   range = 15, pbs = {0.05} },
     -- Yuji
     ["rbxassetid://119657732016011"] = { name = "M1",   range = 15, pbs = {0.05} },
     ["rbxassetid://102160567941101"] = { name = "M2",   range = 15, pbs = {0.05} },
     ["rbxassetid://97112862462365"] = { name = "M3",   range = 15, pbs = {0.05} },
     ["rbxassetid://114035155420354"] = { name = "M4",   range = 15, pbs = {0.05} },
-    ["rbxassetid://116929568448449"] = { name = "Uppercut",   range = 15, pbs = {0.025} },
+    --["rbxassetid://116929568448449"] = { name = "Uppercut",   range = 15, pbs = {0.025} },
     ["rbxassetid://117799573414500"] = { name = "Uptilt",   range = 15, pbs = {0.05} },
-
+    ["rbxassetid://81458066007277"] = { name = "Divergent Fist",   range = 15, pbs = {0.05} },
 }
 
 --["rbxassetid://2222222222"] = { name = "M2",     range = 18, pbs = {0.20, 0.55, 0.90} },
