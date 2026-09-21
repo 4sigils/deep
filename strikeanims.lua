b9bc8537ae7dd001a65c7e3cce03d130868ed929
4sigils <xdmakesmebad@gmail.com>
2026-09-21 09:30:03 -0400

    Update strikeanims.lua

--- strikeanims.lua
+++ strikeanims.lua
@@ -7,7 +7,7 @@ return {
     ["rbxassetid://114035155420354"] = { name = "M4",   range = 15, pbs = {0.04} },
     --["rbxassetid://116929568448449"] = { name = "Uppercut",   range = 15, pbs = {0.025} },
     ["rbxassetid://117799573414500"] = { name = "Uptilt",   range = 15, pbs = {0.05} },
-    ["rbxassetid://81458066007277"] = { name = "Divergent Fist",   range = 15, pbs = {0.1} },
+    ["rbxassetid://81458066007277"] = { name = "Divergent Fist",   range = 15, pbs = {0.3} },
 }
 
 --["rbxassetid://2222222222"] = { name = "M2",     range = 18, pbs = {0.20, 0.55, 0.90} },
