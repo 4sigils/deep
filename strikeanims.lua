41aacba5ac4942478621154ef2cfe844240f3789
4sigils <xdmakesmebad@gmail.com>
2026-09-21 10:12:55 -0400

    Update strikeanims.lua

--- strikeanims.lua
+++ strikeanims.lua
@@ -1,13 +1,20 @@
 return {
     --["rbxassetid://"] = { name = "M1",   range = 15, pbs = {0.05} },
     -- Yuji
-    ["rbxassetid://119657732016011"] = { name = "M1",   range = 15, pbs = {0.04} },
-    ["rbxassetid://102160567941101"] = { name = "M2",   range = 15, pbs = {0.04} },
-    ["rbxassetid://97112862462365"] = { name = "M3",   range = 15, pbs = {0.04} },
-    ["rbxassetid://114035155420354"] = { name = "M4",   range = 15, pbs = {0.04} },
+    ["rbxassetid://119657732016011"] = { name = "M1",   range = 20, pbs = {0.04} },
+    ["rbxassetid://102160567941101"] = { name = "M2",   range = 20, pbs = {0.04} },
+    ["rbxassetid://97112862462365"] = { name = "M3",   range = 20, pbs = {0.04} },
+    ["rbxassetid://114035155420354"] = { name = "M4",   range = 20, pbs = {0.04} },
     --["rbxassetid://116929568448449"] = { name = "Uppercut",   range = 15, pbs = {0.025} },
     ["rbxassetid://117799573414500"] = { name = "Uptilt",   range = 15, pbs = {0.05} },
     ["rbxassetid://81458066007277"] = { name = "Divergent Fist",   range = 15, pbs = {.425, 1} },
+
+    -- Guts
+    ["rbxassetid://106565987725936"] = { name = "M1",   range = 25, pbs = {0.35} },
+    ["rbxassetid://86269757312094"] = { name = "M2",   range = 25, pbs = {0.35} },
+    ["rbxassetid://119740683390972"] = { name = "M3",   range = 25, pbs = {0.35} },
+    ["rbxassetid://122785749937853"] = { name = "M4",   range = 25, pbs = {0.35} },
+    
 }
 
 --["rbxassetid://2222222222"] = { name = "M2",     range = 18, pbs = {0.20, 0.55, 0.90} },
